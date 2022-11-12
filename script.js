8023627c5921e9ce32e1c1516cde5b18db4386c0
GurditLubana <singhg78@myumanitoba.ca>
2022-11-11 20:54:37 -0600

    set Winner

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let winningCombinations = 
+[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [6,4,2],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+]
+
 let currentTurn = "X";
 let box = document.getElementsByClassName("boxes");
 console.log(box);
@@ -6,13 +18,28 @@ for (let i = 0; i < 9; i++) {
 }
 
 function markSymbol() {
-  if (currentTurn === "X") 
+  if (currentTurn === "X" && this.innerText === "") 
   {
     this.innerHTML = "X"
+    checkWinner(currentTurn)
     currentTurn = "O"
   }
-  else{
+  else if(currentTurn === "O" && this.innerText === "")
+  {
     this.innerHTML = "O"
+    checkWinner(currentTurn)
     currentTurn = "X"
   }
+}
+
+function checkWinner(currentTurn)
+{
+    for(let i=0; i<winningCombinations.length; i++)
+    {
+        if(box[winningCombinations[i][0]].innerHTML === currentTurn && box[winningCombinations[i][1]].innerHTML === currentTurn && box[winningCombinations[i][2]].innerHTML === currentTurn)
+        {
+            console.log("winner is " + currentTurn);
+               
+        }
+    }
 }
